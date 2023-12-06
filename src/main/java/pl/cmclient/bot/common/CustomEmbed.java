ce59a82f50acd55159b85ccbbd711b7cb493ece0
shadow <shadow@cmclient.pl>
2023-12-06 22:28:04 +0100

    dependencies update

--- src/main/java/pl/cmclient/bot/common/CustomEmbed.java
+++ src/main/java/pl/cmclient/bot/common/CustomEmbed.java
@@ -7,6 +7,9 @@ import java.awt.*;
 
 public class CustomEmbed {
 
+    private final static Color MAIN_COLOR = new Color(29, 185, 84);
+    private final static Color ERROR_COLOR = new Color(213, 16, 7);
+
     public EmbedBuilder create() {
         EmbedBuilder builder = new EmbedBuilder();
         builder.setFooter(BotApplication.getInstance().getConfig().getBotName() + " | v1.0-SNAPSHOT", BotApplication.getInstance().getApi().getYourself().getAvatar());
@@ -16,7 +19,7 @@ public class CustomEmbed {
 
     public EmbedBuilder create(boolean success) {
         EmbedBuilder builder = this.create();
-        builder.setColor(success ? Color.green : Color.red);
+        builder.setColor(success ? MAIN_COLOR : ERROR_COLOR);
         return builder;
     }
 }
