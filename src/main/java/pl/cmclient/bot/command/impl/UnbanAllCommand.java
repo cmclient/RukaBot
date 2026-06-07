33c49f0e1d5f32dc22ffa40819a06b82db2dd10c
cmclient <support@cmclient.pl>
2026-06-07 21:27:58 +0200

    feat: improved unbanall command

--- src/main/java/pl/cmclient/bot/command/impl/UnbanAllCommand.java
+++ src/main/java/pl/cmclient/bot/command/impl/UnbanAllCommand.java
@@ -15,6 +15,7 @@ import pl.cmclient.bot.helper.StringHelper;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class UnbanAllCommand extends Command {
 
@@ -29,36 +30,83 @@ public class UnbanAllCommand extends Command {
     @Override
     public void execute(SlashCommandInteractionEvent event) {
         OptionMapping acceptOption = event.getOption("accept");
-
-        if (!acceptOption.getAsBoolean()) {
-            event.replyEmbeds(new CustomEmbed()
-                            .create(CustomEmbed.Type.ERROR)
-                            .setTitle("You didn't accepted the question.")
-                            .build())
-                    .setEphemeral(true)
-                    .queue();
-            return;
-        }
-
         Guild guild = event.getGuild();
 
-        guild.retrieveBanList().queue(bans -> event.replyEmbeds(new CustomEmbed()
-                        .create(CustomEmbed.Type.WARNING)
-                        .setTitle("Removing " + bans.size() + " bans...")
-                        .build())
-                //.setEphemeral(true)
-                .queue(interactionHook -> {
-                    Instant start = Instant.now();
-                    bans.forEach(ban -> guild.unban(ban.getUser()).queue());
-                    Instant end = Instant.now();
-                    Duration duration = Duration.between(start, end);
-                    String timeElapsed = StringHelper.formatDuration(duration);
+        guild.retrieveBanList().queue(bans -> {
+            if (!acceptOption.getAsBoolean()) {
+                event.replyEmbeds(new CustomEmbed()
+                                .create(CustomEmbed.Type.ERROR)
+                                .setTitle("You didn't accept the question.")
+                                .setDescription("There are **" + bans.size() + "** banned users.")
+                                .build())
+                        .setEphemeral(true)
+                        .queue();
+                return;
+            }
 
-                    interactionHook.editOriginalEmbeds(new CustomEmbed()
-                            .create(CustomEmbed.Type.SUCCESS)
-                            .setTitle("Completed, removed " + bans.size() + " bans.")
-                            .addField("Time Elapsed", timeElapsed, false)
-                            .build()).queue();
-                }));
+            int totalBans = bans.size();
+            if (totalBans == 0) {
+                event.replyEmbeds(new CustomEmbed()
+                                .create(CustomEmbed.Type.WARNING)
+                                .setTitle("No banned users found.")
+                                .build())
+                        .setEphemeral(true)
+                        .queue();
+                return;
+            }
+
+            event.replyEmbeds(new CustomEmbed()
+                            .create(CustomEmbed.Type.WARNING)
+                            .setTitle("Removing " + totalBans + " bans...")
+                            .setDescription("Progress: 0/" + totalBans)
+                            .build())
+                    .queue(interactionHook -> {
+                        Instant start = Instant.now();
+                        AtomicInteger unbannedCount = new AtomicInteger(0);
+                        AtomicInteger lastUpdate = new AtomicInteger(0);
+
+                        bans.forEach(ban -> guild.unban(ban.getUser()).queue(
+                                success -> {
+                                    int current = unbannedCount.incrementAndGet();
+                                    
+                                    if (current - lastUpdate.get() >= 10 || current == totalBans) {
+                                        lastUpdate.set(current);
+                                        
+                                        if (current == totalBans) {
+                                            Instant end = Instant.now();
+                                            Duration duration = Duration.between(start, end);
+                                            String timeElapsed = StringHelper.formatDuration(duration);
+
+                                            interactionHook.editOriginalEmbeds(new CustomEmbed()
+                                                    .create(CustomEmbed.Type.SUCCESS)
+                                                    .setTitle("Completed, removed " + totalBans + " bans.")
+                                                    .addField("Time Elapsed", timeElapsed, false)
+                                                    .build()).queue();
+                                        } else {
+                                            interactionHook.editOriginalEmbeds(new CustomEmbed()
+                                                    .create(CustomEmbed.Type.WARNING)
+                                                    .setTitle("Removing " + totalBans + " bans...")
+                                                    .setDescription("Progress: " + current + "/" + totalBans)
+                                                    .build()).queue();
+                                        }
+                                    }
+                                },
+                                error -> {
+                                    int current = unbannedCount.incrementAndGet();
+                                    if (current == totalBans) {
+                                        Instant end = Instant.now();
+                                        Duration duration = Duration.between(start, end);
+                                        String timeElapsed = StringHelper.formatDuration(duration);
+
+                                        interactionHook.editOriginalEmbeds(new CustomEmbed()
+                                                .create(CustomEmbed.Type.SUCCESS)
+                                                .setTitle("Completed, removed " + totalBans + " bans.")
+                                                .addField("Time Elapsed", timeElapsed, false)
+                                                .build()).queue();
+                                    }
+                                }
+                        ));
+                    });
+        });
     }
 }
